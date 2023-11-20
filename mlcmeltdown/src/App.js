bed17a8048d84f31bdf82a2b9da98662a98838b4
Manav Patel <mpp67530@uga.edu>
2023-11-20 15:49:47 -0500

    Added authenticated user view

--- mlcmeltdown/src/App.js
+++ mlcmeltdown/src/App.js
@@ -1,17 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import TicketForm from './components/TicketForm';
+import HomePage from './components/HomePage';
 import './App.css';
+
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route exact path='/' element={<p>Thing</p>} />
-          <Route path='/create-ticket' element={<TicketForm />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route exact path='/' element={<p>thing</p>} />
+        <Route path = '/Home-Page' element = {<HomePage />} />
+        <Route path='/create-ticket' element={<TicketForm />} />
+      </Routes>
     </Router>
   );
 };
---
+++ mlcmeltdown/src/components/HomePage.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './HomePage.css';
+
+const HomePage = () => {
+    const Floor = ({ number, children }) => (
+        <div className="floor">
+          <h2>Floor {number}</h2>
+          {children}
+        </div>
+      );
+      
+      const Room = ({ number }) => {
+        const [isReserved, setIsReserved] = React.useState(false);
+      
+        const handleReserve = () => {
+          setIsReserved(true);
+          // Additional logic to handle reservation...
+        };
+      
+        return (
+          <div className={`room ${isReserved ? 'reserved' : ''}`}>
+            <span>Room {number}</span>
+            <button onClick={handleReserve} disabled={isReserved}>
+              {isReserved ? 'Reserved' : 'Reserve'}
+            </button>
+          </div>
+        );
+      };
+      
+      const SignOutButton = () => {
+        // Logic for signing out...
+        return (
+          <button className="sign-out">Sign Out</button>
+        );
+      };
+    return (
+      <div className="app">
+        {/* <Header title="MLC Meltdown" /> */}
+        <Floor number="1">
+          <Room number="124" />
+          <Room number="125" />
+        </Floor>
+        <Floor number="2">
+          <Room number="224" />
+          <Room number="225" />
+        </Floor>
+        <Floor number="3">
+          <Room number="324" />
+          <Room number="325" />
+        </Floor>
+        <Floor number="4">
+          <Room number="424" />
+          <Room number="425" />
+        </Floor>
+        <SignOutButton />
+      </div>
+    );
+  }
+  export default HomePage;
+//   function Floor({ number, children }) {
+//     return (
+//       <div className="floor">
+//         <h2>Floor {number}</h2>
+//         {children}
+//       </div>
+//     );
+//   }
+  
+//   function Room({ number }) {
+//     const [isReserved, setIsReserved] = React.useState(false);
+  
+//     function handleReserve() {
+//       setIsReserved(true);
+//       // Additional logic to handle reservation...
+//     }
+  
+//     return (
+//       <div className={`room ${isReserved ? 'reserved' : ''}`}>
+//         <span>Room {number}</span>
+//         <button onClick={handleReserve} disabled={isReserved}>
+//           {isReserved ? 'Reserved' : 'Reserve'}
+//         </button>
+//       </div>
+//     );
+//   }
+  
+//   function SignOutButton() {
+//     // Logic for signing out...
+//     return (
+//       <button className="sign-out">Sign Out</button>
+//     );
+//   }
+  
+
