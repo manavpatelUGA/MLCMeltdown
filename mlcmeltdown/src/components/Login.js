f0d16620c68083006c590d0b6e47089c585ced8e
Manav Patel <mpp67530@uga.edu>
2023-11-20 22:00:28 -0500

    login page updates

--- mlcmeltdown/src/components/Login.js
+++ mlcmeltdown/src/components/Login.js
@@ -2,16 +2,17 @@
 import React from 'react';
 import './Login.css'; // Make sure to create a corresponding CSS file
 import { Link } from 'react-router-dom';
+import logo from './logo.jpg'
 
 const Login = () => {
     var isLoggedIn = false;
     return (
     <div className="login-container">
-      <h1>Welcome</h1>
+      <h1>MLC Meltown</h1>
       <div className="icon-container">
         {/* SVG or image tag for the icon */}
         {/* Replace the src with your actual icon path */}
-        <img src="mlcmeltdown\src\components\logo.svg" alt="Login Icon" />
+        <img src={logo} alt="Login Icon" style={{height: '100px', width: '100px'}}/>
       </div>
       <form className="login-form">
         <input type="text" placeholder="Student ID" />
